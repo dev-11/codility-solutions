fb8a294ab7dbce4e060cfbd8f488245185aabeb9
Otto <otto@masterbranch.io>
2019-09-25 22:20:47 +0100

    adding TapeEquilibrium solution

---
+++ Lesson_03/tape_equilibrium.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def solution(A):
+    left = sum(A[1:])
+    right = A[0]
+    m = abs(left - right)
+    for _ in range(1, len(A)-1):
+        left -= A[_]
+        right += A[_]
+        m = min(abs(left - right), m)
+        
+    return m
+
