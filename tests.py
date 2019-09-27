16f89329eb8dc766f8c87aaec9066fdfa858892f
Otto <otto@masterbranch.io>
2019-09-27 17:25:05 +0100

    adding lesson 04 tests

--- tests.py
+++ tests.py
@@ -58,5 +58,23 @@ class Lesson03Tests(unittest.TestCase):
         self.assertEqual(1, res)
 
 
+class Lesson04Tests(unittest.TestCase):
+    def test_frog_river_one_sample_test_01(self):
+        x = 5
+        a = [1, 3, 1, 4, 2, 3, 5, 4]
+        res = Lesson04.frog_river_one(x, a)
+        self.assertEqual(6, res)
+
+    def test_perm_check_sample_test_01(self):
+        a = [4, 1, 3, 2]
+        res = Lesson04.perm_check(a)
+        self.assertEqual(1, res)
+
+    def test_perm_check_sample_test_02(self):
+        a = [4, 1, 3]
+        res = Lesson04.perm_check(a)
+        self.assertEqual(0, res)
+
+
 if __name__ == '__main__':
     unittest.main()
