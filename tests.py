83ee0e83d7d9a85687a53a7238a6d4b73c6a973b
Otto <otto@masterbranch.io>
2019-09-27 17:43:18 +0100

    lesson 06 tests added

--- tests.py
+++ tests.py
@@ -83,7 +83,26 @@ class Lesson05Tests(unittest.TestCase):
         self.assertEqual(5, res)
 
 
-# class Lesson06Tests(unittest.TestCase):
+class Lesson06Tests(unittest.TestCase):
+    def test_max_product_of_three_test_01(self):
+        a = [-3, 1, 2, -2, 5, 6]
+        res = Lesson06.max_product_of_three(a)
+        self.assertEqual(res, 60)
+
+    def test_distinct_test_01(self):
+        a = [2, 1, 1, 2, 3, 1]
+        res = Lesson06.distinct(a)
+        self.assertEqual(res, 3)
+
+    def test_triangle_test_01(self):
+        a = [10, 2, 5, 1, 8, 20]
+        res = Lesson06.triangle(a)
+        self.assertEqual(res, 1)
+
+    def test_triangle_test_02(self):
+        a = [10, 2, 5, 1]
+        res = Lesson06.triangle(a)
+        self.assertEqual(res, 0)
 
 
 class Lesson07Tests(unittest.TestCase):
@@ -106,7 +125,6 @@ class Lesson07Tests(unittest.TestCase):
         s = "())"
         res = Lesson07.nesting(s)
         self.assertEqual(res, 0)
-
 
 
 class Lesson08Tests(unittest.TestCase):
