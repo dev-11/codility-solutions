6a100889ec60e2c49a404c524e256c9e0d5e1e31
Otto <otto@masterbranch.io>
2019-09-27 17:19:46 +0100

    adding lesson 03 tests

--- Solutions/Lesson_03/frog_jmp.py
+++ Solutions/Lesson_03/frog_jmp.py
@@ -2,5 +2,5 @@ import math
 
 
 def solution(X, Y, D):
-    return math.ceil((Y - X) / D)
+    return math.ceil((Y-X)/float(D))
 
--- tests.py
+++ tests.py
@@ -39,5 +39,24 @@ class Lesson02Tests(unittest.TestCase):
         self.assertEqual(7, res)
 
 
+class Lesson03Tests(unittest.TestCase):
+    def test_from_jump_sample_test_01(self):
+        x = 10
+        y = 85
+        d = 30
+        res = Lesson03.frog_jmp(x, y, d)
+        self.assertEqual(3, res)
+
+    def test_perm_missing_elem(self):
+        a = [2, 3, 1, 5]
+        res = Lesson03.perm_missing_elem(a)
+        self.assertEqual(4, res)
+
+    def test_tape_equilibrium(self):
+        a = [3, 1, 2, 4, 3]
+        res = Lesson03.tape_equilibrium(a)
+        self.assertEqual(1, res)
+
+
 if __name__ == '__main__':
     unittest.main()
