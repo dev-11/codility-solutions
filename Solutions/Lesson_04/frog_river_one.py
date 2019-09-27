3b49b2a4490a6bc241ed5e80bae823742b5ea681
Otto <otto@masterbranch.io>
2019-09-27 17:59:22 +0100

    removing unused item

--- Solutions/Lesson_04/frog_river_one.py
+++ Solutions/Lesson_04/frog_river_one.py
@@ -1,12 +1,12 @@
 def solution(X, A):
     B = [False] * max(A)
     counter = 0
-    for _, elem in enumerate(A):
-        idx = A[_] - 1
+    for index, _ in enumerate(A):
+        idx = A[index] - 1
         if not B[idx]:
             B[idx] = True
             counter = counter + 1
         if counter == X:
-            return _
+            return index
     return -1
 
