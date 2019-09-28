32cc886eddd61776a7cc980daaaeebd575983008
Otto <otto@masterbranch.io>
2019-09-28 12:40:52 +0100

    :white_check_mark: stone wall sample test added

--- Tests/lesson07_tests.py
+++ Tests/lesson07_tests.py
@@ -23,3 +23,8 @@ class Lesson07Tests(unittest.TestCase):
         res = Lesson07.nesting(s)
         self.assertEqual(res, 0)
 
+    def test_stone_wall_sample_test01(self):
+        h = [8, 8, 5, 7, 9, 8, 4, 8]
+        res = Lesson07.stone_wall(h)
+        self.assertEqual(7, res)
+
