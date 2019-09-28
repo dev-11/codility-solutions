2b5613116a8d91b1936fab910ef88e66422c0bf7
Otto <otto@masterbranch.io>
2019-09-28 10:12:36 +0100

    more simple code

--- Solutions/Lesson_07/stone_wall.py
+++ Solutions/Lesson_07/stone_wall.py
@@ -3,14 +3,13 @@ def solution(H):
     wall_position = 0
     used_blocks = 0
 
-    for _, current_height in enumerate(H):
-        while wall_position > 0 and wall[wall_position - 1] > current_height:
+    for height in H:
+        while wall_position > 0 and wall[wall_position - 1] > height:
             wall_position -= 1
-        if wall_position == 0 or wall[wall_position - 1] != current_height:
-            wall[wall_position] = current_height
+        if wall_position < 1 or wall[wall_position - 1] != height:
+            wall[wall_position] = height
             wall_position += 1
             used_blocks += 1
 
     return used_blocks
-
 
