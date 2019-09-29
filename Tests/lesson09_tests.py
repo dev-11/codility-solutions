ac2b80e22b45528413934b4991ff8c771e9b11d6
Otto <otto@masterbranch.io>
2019-09-29 05:08:55 +0100

    :fire:removing trailing white space

--- Tests/lesson09_tests.py
+++ Tests/lesson09_tests.py
@@ -7,7 +7,7 @@ class Lesson09Tests(unittest.TestCase):
         a = [23171, 21011, 21123, 21366, 21013, 21367]
         res = Lesson09.max_profit(a)
         self.assertEqual(res, 356)
-        
+
     def test_max_slice_sum_sample_test_01(self):
         a = [3, 2, -6, 4, 0]
         res = Lesson09.max_slice_sum(a)
