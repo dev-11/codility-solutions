4cecf8ca397445908efa7f6b29cfc3fca4580e53
Otto <otto@masterbranch.io>
2019-09-29 05:46:56 +0100

    max slice sum solution update

--- Solutions/Lesson_09/max_slice_sum.py
+++ Solutions/Lesson_09/max_slice_sum.py
@@ -1,8 +1,8 @@
 def solution(A):
-    slice_sum = 0
-    max_sum = 0
-    for _ in A:
-        slice_sum += _
+    slice_sum = A[0]
+    max_sum = A[0]
+    for _ in range(1, len(A)):
+        slice_sum = max(A[_], slice_sum + A[_])
         max_sum = max(max_sum, slice_sum)
     return max_sum
 
