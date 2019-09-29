1cc4e16f1c82ae6b38dcd40451792eea55871cba
Otto <otto@masterbranch.io>
2019-09-29 16:19:48 +0100

    test rename

--- Tests/lesson09_tests.py
+++ Tests/lesson09_tests.py
@@ -3,7 +3,7 @@ from Solutions import Lesson09
 
 
 class Lesson09Tests(unittest.TestCase):
-    def test_max_profit_sampletest_01(self):
+    def test_max_profit_sample_test_01(self):
         a = [23171, 21011, 21123, 21366, 21013, 21367]
         res = Lesson09.max_profit(a)
         self.assertEqual(res, 356)
