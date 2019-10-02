22e49fc0bc0731b98e92a676c0c31f034c41a557
Otto <otto@masterbranch.io>
2019-10-02 22:54:36 +0100

    Update lesson10_tests.py

--- Tests/Training/lesson10_tests.py
+++ Tests/Training/lesson10_tests.py
@@ -2,7 +2,7 @@ import unittest
 from Solutions import Lesson10
 
 
-class Lesson09Tests(unittest.TestCase):
+class Lesson10Tests(unittest.TestCase):
     def test_count_factors_example_test_01():
         n = 24
         res = Lesson10.count_factors(n)
