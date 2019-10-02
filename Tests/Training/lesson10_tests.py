d628db2d1e8cd44c24534f833c19257b467d37d1
Otto <otto@masterbranch.io>
2019-10-02 22:58:11 +0100

    fixing build brake

--- Tests/Training/lesson10_tests.py
+++ Tests/Training/lesson10_tests.py
@@ -3,7 +3,7 @@ from Solutions import Lesson10
 
 
 class Lesson10Tests(unittest.TestCase):
-    def test_count_factors_example_test_01():
+    def test_count_factors_example_test_01(self):
         n = 24
         res = Lesson10.count_factors(n)
         self.assertEqual(res, 8)
