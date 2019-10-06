a2718cc133c72bb2545315c26fffb0377e354eb4
Otto <otto@masterbranch.io>
2019-10-06 08:29:02 +0100

    chocolates_by_numbers.py Update

--- Solutions/Training/Lesson_12/chocolates_by_numbers.py
+++ Solutions/Training/Lesson_12/chocolates_by_numbers.py
@@ -1,9 +1,6 @@
+import math
+
+
 def solution(N, M):
-    empty_wrappers = []
-    i = 0
+    return N // math.gcd(N, M)
 
-    while i not in empty_wrappers:
-        empty_wrappers.append(i)
-        i = (i + M) % N
-
-    return len(empty_wrappers)
