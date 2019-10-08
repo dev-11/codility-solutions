82241ee0d15af24ccb1ed7cd7c48e1207a4a5eba
Otto <otto@masterbranch.io>
2019-10-08 23:32:59 +0100

    :white_check_mark: unit test fix

--- Tests/Training/lesson15_tests.py
+++ Tests/Training/lesson15_tests.py
@@ -17,10 +17,10 @@ class Lesson15Tests(unittest.TestCase):
     def test_count_triangles_test_01(self):
         a = [5, 3, 2]
         res = Lesson15.count_triangles(a)
-        self.assertEqual(4, res)
+        self.assertEqual(0, res)
 
     def test_count_triangles_test_02(self):
         a = [3, 3, 5, 6]
         res = Lesson15.count_triangles(a)
-        self.assertEqual(4, res)
+        self.assertEqual(3, res)
 
