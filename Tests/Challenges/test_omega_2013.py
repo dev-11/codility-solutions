191695bac206e9482ccf8dac856804dfe9840de6
Otto <otto@masterbranch.io>
2019-10-09 12:23:45 +0100

    Update and rename omega_2013_tests.py to test_omega_2013.py

--- Tests/Challenges/test_omega_2013.py
+++ Tests/Challenges/test_omega_2013.py
@@ -2,7 +2,7 @@ import unittest
 from Solutions import Challenges
 
 
-class ChallengesTests(unittest.TestCase):
+class Omega2013Tests(unittest.TestCase):
     def test_omega_2013_example_test_01(self):
         a = [5, 6, 4, 3, 6, 2, 3]
         b = [2, 3, 5, 2, 4]
