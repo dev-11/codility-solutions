b7b3b1ad6a384d7f9fe61f4fba372b4666d7e0c1
Otto <otto@masterbranch.io>
2019-10-19 23:29:29 +0100

    count_non_divisible.py empty method added

--- Solutions/Training/Lesson_11/__init__.py
+++ Solutions/Training/Lesson_11/__init__.py
@@ -1,2 +1,3 @@
 from .count_semiprimes import solution as count_semiprimes
+from .count_non_divisible import solution as count_non_divisible
 
---
+++ Solutions/Training/Lesson_11/count_non_divisible.py
@@ -0,0 +1,6 @@
+def solution(A):
+    return 0
+
+print(solution([1,2,3,4,5]))
+print(solution(12345))
+
